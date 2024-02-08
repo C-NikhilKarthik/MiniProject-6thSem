482a9e15e38753c2b9035513f4c0f97cb8f57474
Anand930singh <anand2002king@gmail.com>
2024-02-08 11:05:13 +0530

    minor changes

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 
 const PatternBg = {uri:'https://e1.pxfuel.com/desktop-wallpaper/759/194/desktop-wallpaper-subtle-pride-phone-background-made-by-me-r-lgbt-thumbnail.jpg'}
 
@@ -13,7 +14,7 @@ function LandingPage() {
   return (
     <ImageBackground source={PatternBg} resizeMode='cover' className='h-full w-full  flex-none justify-center items-center rotate-180'>      
         <View className="h-full w-full flex-none justify-center items-center rotate-180">
-            <Text className="text-white text-4xl tracking-widest" style={[{fontFamily:"ProtestStrike-Regular"}]} >SNAPMARK</Text>
+            <Text className="text-white text-4xl tracking-widest" >SNAPMARK</Text>
             <StatusBar style="auto" />
         </View>
       </ImageBackground>
